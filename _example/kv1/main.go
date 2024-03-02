db47ee6483eeb8a217232c33491c83ab7c12fc32
kevburnsjr <kevburnsjr@gmail.com>
2024-03-02 03:17:36 -0800

    Refactor exmaple config

--- _example/kv1/main.go
+++ _example/kv1/main.go
@@ -15,16 +15,17 @@ import (
 )
 
 var (
-	name       = flag.String("n", `kv1`, "Cluster name (base36 maxlen 12)")
-	peers      = flag.String("p", `kv1-usc1-a-0,kv1-usc1-c-0,kv1-usc1-f-0`, "Peer nodes")
-	region     = flag.String("re", `us-central1`, "Region")
-	zone       = flag.String("z", `us-central1-a`, "Zone")
-	shards     = flag.Int("s", 4, "Shard count")
-	apiAddr    = flag.String("a", "10.0.0.1:17001", "Internal gRPC api address")
+	gossipAddr = flag.String("g", "10.0.0.1:17001", "Memberlist gossip address")
 	raftAddr   = flag.String("r", "10.0.0.1:17002", "Dragonboat raft address")
-	gossipAddr = flag.String("g", "10.0.0.1:17003", "Memberlist gossip address")
+	zongziAddr = flag.String("a", "10.0.0.1:17003", "Zongzi gRPC api address")
 	httpAddr   = flag.String("h", "10.0.0.1:8000", "HTTP address")
-	dataDir    = flag.String("d", "/var/lib/zongzi", "Base data directory")
+
+	name    = flag.String("n", `kv1`, "Cluster name (base36 maxlen 12)")
+	peers   = flag.String("p", `kv1-usc1-a-0,kv1-usc1-c-0,kv1-usc1-f-0`, "Peer nodes")
+	region  = flag.String("re", `us-central1`, "Region")
+	zone    = flag.String("z", `us-central1-a`, "Zone")
+	dataDir = flag.String("d", "/var/lib/zongzi", "Base data directory")
+	shards  = flag.Int("s", 4, "Shard count")
 )
 
 func main() {
@@ -33,19 +34,19 @@ func main() {
 	ctx := context.Background()
 	ctrl := newController()
 	agent, err := zongzi.NewAgent(*name, strings.Split(*peers, ","),
-		zongzi.WithHostTags(
-			fmt.Sprintf(`geo:region=%s`, *region),
-			fmt.Sprintf(`geo:zone=%s`, *zone)),
-		zongzi.WithApiAddress(*apiAddr),
-		zongzi.WithGossipAddress(*gossipAddr),
 		zongzi.WithHostConfig(zongzi.HostConfig{
 			NodeHostDir:    *dataDir + "/raft",
 			NotifyCommit:   true,
-			RaftAddress:    *raftAddr,
 			RTTMillisecond: 2,
 			WALDir:         *dataDir + "/wal",
 		}),
-		zongzi.WithRaftEventListener(ctrl))
+		zongzi.WithGossipAddress(*gossipAddr),
+		zongzi.WithRaftAddress(*raftAddr),
+		zongzi.WithApiAddress(*zongziAddr),
+		zongzi.WithRaftEventListener(ctrl),
+		zongzi.WithHostTags(
+			fmt.Sprintf(`geo:region=%s`, *region),
+			fmt.Sprintf(`geo:zone=%s`, *zone)))
 	if err != nil {
 		panic(err)
 	}
