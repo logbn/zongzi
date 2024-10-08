6e1f97f37061952b4e1532fbb77f7b20852cc5f9
kevburnsjr <kevburnsjr@gmail.com>
2024-10-08 06:06:24 -0700

    Fix kv example

--- _example/kv1/main.go
+++ _example/kv1/main.go
@@ -33,8 +33,8 @@ func main() {
 	zongzi.SetLogLevelDebug()
 	ctx := context.Background()
 	agent, err := zongzi.NewAgent(*name, strings.Split(*peers, ","),
-		zongzi.WithRaftDir(dataDir+"/raft"),
-		zongzi.WithWALDir(dataDir+"/wal"),
+		zongzi.WithRaftDir(*dataDir+"/raft"),
+		zongzi.WithWALDir(*dataDir+"/wal"),
 		zongzi.WithGossipAddress(*gossipAddr),
 		zongzi.WithRaftAddress(*raftAddr),
 		zongzi.WithApiAddress(*zongziAddr),
